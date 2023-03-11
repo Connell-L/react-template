7ebd4d4ad85d61742229413b7efb91bec6d0e3a7
Louise Connell <louise.connell@olmgroup.com>
2023-03-11 11:32:30 +0000

    Custom webpack, babel, eslint, prettier with typescript react

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
 const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: './js/main.js',  },  
-  resolve: {
-    extensions: ['.js', '.jsx']
+    filename: './js/main.js',
   },
- module: {
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css', '.scss', '.sass', '.less', '.styl', '.png', '.jpg', '.jpeg', '.gif', '.svg', '.woff', '.woff2', '.ttf', '.eot', '.otf']
+  },
+  module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         use: [
-          {loader: 'babel-loader'}
+          { loader: 'babel-loader' }
         ]
       }
     ]
@@ -24,16 +26,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    })
+    }),
+    new ForkTsCheckerWebpackPlugin(),
   ],
   devServer: {
-    port: 9000,
+    port: 3000,
     static: {
       directory: path.join(__dirname, 'dist'),
     },
   },
-  
-  devtool: 'source-map'
-}
-  
-
+  devtool: 'source-map',
+};
